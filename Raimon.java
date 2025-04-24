2ece15a802fd2327fa012b75dbefdf1242c0b126
sergio-sudo-debug <sergio.ruiz@plazacastilla.salesianas.org>
2025-04-24 12:14:49 +0200

    Add files via upload

---
+++ Raimon.java
@@ -0,0 +1,12 @@
+package sergio_ruiz;
+
+public class Raimon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola este es mi primer programa :D");
+		
+
+	}
+
+}
